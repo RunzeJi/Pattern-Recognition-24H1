3fc0f4e2339ed779d52a303e035265ec326a2900
Runze Ji <jirunzeamerica@icloud.com>
2024-04-15 10:49:05 +0800

    Update trajectory.py

--- trajectory.py
+++ trajectory.py
@@ -1,20 +1,27 @@
-import os
+import os, platform
 import pandas as pd
 from tqdm import tqdm
 from itertools import islice
 from matplotlib import pyplot as plt
 
-
-dict = {
+type_dict = {
     '围网':0,
     '刺网':1,
     '拖网':2
     }
 
 path_dict = {
-    0:f'/Users/macosexternal/Desktop/PR/classification/custom_dataset/cat0/',
-    1:f'/Users/macosexternal/Desktop/PR/classification/custom_dataset/cat1/',
-    2:f'/Users/macosexternal/Desktop/PR/classification/custom_dataset/cat2/'
+    'Windows':{0:f'../../PR/classification/custom_dataset/cat0/',
+               1:f'../../PR/classification/custom_dataset/cat1/',
+               2:f'../../PR/classification/custom_dataset/cat2/'},
+               
+    'Linux':{0:f'../../PR/classification/custom_dataset/cat0/',
+             1:f'../../PR/classification/custom_dataset/cat1/',
+             2:f'../../PR/classification/custom_dataset/cat2/'},
+
+    'Darwin':{0:f'/Users/macosexternal/Desktop/PR/classification/custom_dataset/cat0/',
+              1:f'/Users/macosexternal/Desktop/PR/classification/custom_dataset/cat1/',
+              2:f'/Users/macosexternal/Desktop/PR/classification/custom_dataset/cat2/'},
     }
 
 BASE_PATH = '/Users/macosexternal/Desktop/PR/train/'
@@ -24,15 +31,15 @@ def get_trace_img(count):
     file_df.drop(['渔船ID','time','速度','方向'], axis=1, inplace=True)
     file_df.head()
 
-    cat = dict[file_df['type'].iloc[0]]
+    cat = type_dict[file_df['type'].iloc[0]]
     #print(cat)
 
     plt.scatter(file_df['lon'], file_df['lat'], s=1)
     #plt.show()
 
-
-    plt.savefig(f'{path_dict[cat]}{count}.jpg', dpi=250)
+    plt.savefig(f'{path_dict[platform.system()][cat]}{count}.jpg', dpi=250)
     plt.clf()
 
 for count in tqdm(range(1,18329), 'plotting and saving'):
-    get_trace_img(count)
+    #get_trace_img(count)
+    pass
